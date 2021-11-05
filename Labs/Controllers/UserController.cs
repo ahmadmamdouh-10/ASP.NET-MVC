ef8489d68055b392336d1e6b06f1ba06b9279335
a-mamdouh <ahmadmamdouh3@gmail.com>
2021-11-05 18:58:04 +0200

    Add project files.

---
+++ Labs/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using Labs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Labs.Controllers
+{
+    public class UserController : Controller
+    {
+        private static List<User> ListOfUsers
+            = new List<User>()
+            {
+                new User(){ID=100,FirstName="Ahmed",LastName="Mamdouh",Email="ahmadmamdouh3.gmail.com",
+                UserName="a-mamdouh",Password="123456",Gender=false},
+
+                new User(){ID=101,FirstName="Naira",LastName="Anwar",Email="Naira0anwar.gmail.com",
+                UserName="naira.Anwar1",Password="123456",Gender=true}
+            };
+            
+
+        // GET: User
+        public ActionResult Index()
+        {
+            return View(ListOfUsers);
+        }
+
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(User u)
+        {
+            ListOfUsers.Add(u);
+            return Redirect("/User/Index");
+        }
+    }
+}
